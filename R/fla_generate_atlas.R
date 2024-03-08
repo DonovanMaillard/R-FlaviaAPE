30bd7d71564c1e3daeb9524e4215ffd1e5cdc6c2
DonovanMaillard <donovan.maillard@gmail.com>
2024-03-08 08:24:20 +0100

    Update fla_generate_atlas.R

--- R/fla_generate_atlas.R
+++ R/fla_generate_atlas.R
@@ -62,8 +62,7 @@ fla_generate_atlas <- function(df=data_sp, territory=NULL, url_basemap=NULL, wkt
     if (label) {
       map<-addControl(map, html=df_iter[[1, label_field]], position = "topright")
     }
-    # Attente 30secondes pour le chargement du WMS
-    Sys.sleep(30)
+
     # Imprimer chaque carte
     filename<-paste("atlas/",iterations[i],".png")
     mapshot(map, file = filename, cliprect = "viewport", remove_controls=c("zoomControl", "layersControl","homeButton") )
