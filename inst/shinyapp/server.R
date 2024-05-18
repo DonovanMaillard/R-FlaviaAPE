0df6a48cd52bb8fb5ed735281d568c6289345f49
DonovanMaillard <donovan.maillard@gmail.com>
2024-05-18 17:02:45 +0200

    test

--- inst/shinyapp/server.R
+++ inst/shinyapp/server.R
@@ -20,26 +20,7 @@ options(shiny.maxRequestSize = 100*1024^2)  # 100 Mo en bytes
 function(input, output) {
   # Stocker le fichier source chargé dans un dataframe
   observeEvent(input$goButton, {
-    file <- req(input$csv_flavia_file)  # Correction du nom de l'input (utilisez des underscores)
-    if (is.null(file))
-      print("Data is NULL")
-      return(NULL)
-    ext <- tools::file_ext(file$name)
-    print(paste("File extension:", ext))
-    if (ext == "csv") {
-      read.csv2(file$datapath, header = input$header)
-    } else {
-      stop("Type de fichier non supporté.")
-    }
-
-    output$nb_data <- renderText({
-    # Utiliser nb_data() pour obtenir la valeur de l'expression réactive
-    	paste("Nombre total de données : ", nrow(data()))
-  	})
-
-    output$contents <- renderTable({
-    	data()
-  	})
+    token <- paste("La token est", input$token-lifetraits)
   })
   
 }
