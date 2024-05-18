57e7a5b7b9d01bc59a2fd8438a04dc32ea7dfd9d
DonovanMaillard <donovan.maillard@gmail.com>
2024-05-18 16:57:28 +0200

    Update server.R

--- inst/shinyapp/server.R
+++ inst/shinyapp/server.R
@@ -19,7 +19,7 @@ options(shiny.maxRequestSize = 100*1024^2)  # 100 Mo en bytes
 
 function(input, output) {
   # Stocker le fichier source chargé dans un dataframe
-  data <- reactive({
+  observeEvent(input$goButton, {
     file <- req(input$csv_flavia_file)  # Correction du nom de l'input (utilisez des underscores)
     if (is.null(file))
       print("Data is NULL")
@@ -31,24 +31,15 @@ function(input, output) {
     } else {
       stop("Type de fichier non supporté.")
     }
-  })
 
-  # Calculer le nombre de données
-  nb_data <- reactive({
-    if (is.null(data())) {
-      return(0)  # retourner 0 si les données sont NULL
-    } else {
-      return(nrow(data()))  # utiliser nrow pour obtenir le nombre de lignes
-    }
+    output$nb_data <- renderText({
+    # Utiliser nb_data() pour obtenir la valeur de l'expression réactive
+    	paste("Nombre total de données : ", nrow(data()))
+  	})
+
+    output$contents <- renderTable({
+    	data()
+  	})
   })
   
-  # Afficher le nombre total de données dans la UI
-  output$nb_data <- renderText({
-    # Utiliser nb_data() pour obtenir la valeur de l'expression réactive
-    paste("Nombre total de données : ", nb_data())
-  })
-
-  output$contents <- renderTable({
-    data()
-  })
 }
