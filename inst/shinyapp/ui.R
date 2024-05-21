63d22c9c947caba261fae8b218b677813b85e87b
DonovanMaillard <donovan.maillard@gmail.com>
2024-05-21 09:21:12 +0200

    test UI

--- inst/shinyapp/ui.R
+++ inst/shinyapp/ui.R
@@ -25,6 +25,9 @@ dashboardPage(
             # First tab content
             tabItem(tabName = "report",
                     h1("Générer un rapport automatique"),
+                    #
+                    # DONNEES SOURCES - INPUTS 
+                    #
                     fluidRow(
                         box(title = "Données sources", status = "primary", solidHeader = TRUE, width = 12,
                             fluidRow(
@@ -34,9 +37,89 @@ dashboardPage(
                             )
                         )
                     ),
+                    # 
+                    # RESULTATS - OUTPUTS
+                    # 
                     fluidRow(
                         box(title = "Statistiques globales", status = "success", solidHeader = TRUE, width = 12,
-                            textOutput(outputId="nb_species")
+                            div(
+                                fluidRow(
+                                    column(4, 
+                                        card(
+                                            card_header(
+                                                class = "bg-dark",
+                                                "Nombre de données"
+                                            ),
+                                            card_body(
+                                                h3(textOutput(outputId="nb_species"))
+                                            )
+                                        )
+                                    ),
+                                    column(4, 
+                                        card(
+                                            card_header(
+                                                class = "bg-dark",
+                                                "Nombre de taxons"
+                                            ),
+                                            card_body(
+                                                h3(textOutput(outputId="nb_species")),
+                                                br(),
+                                                h4("Dont X espèces et Y sous-espèces")
+                                            )
+                                        )
+                                        ),
+                                    column(4, 
+                                        card(
+                                            card_header(
+                                                class = "bg-dark",
+                                                "Nombre de relevés"
+                                            ),
+                                            card_body(
+                                                h3(textOutput(outputId="nb_species"))
+                                            )
+                                        )
+                                        )
+                                    ),
+                            ),
+                            div(
+                                fluidRow(
+                                    column(4, 
+                                        card(
+                                            card_header(
+                                                class = "bg-dark",
+                                                "Nombre de sessions de prospections"
+                                            ),
+                                            card_body(
+                                                h3(textOutput(outputId="nb_species"))
+                                            )
+                                        )
+                                    ),
+                                    column(4, 
+                                        card(
+                                            card_header(
+                                                class = "bg-dark",
+                                                "Méthodes utilisées"
+                                            ),
+                                            card_body(
+                                                h3(textOutput(outputId="nb_species")),
+                                                br(),
+                                                h4("Dont X espèces et Y sous-espèces")
+                                            )
+                                        )
+                                        ),
+                                    column(4, 
+                                        card(
+                                            card_header(
+                                                class = "bg-dark",
+                                                "Autre info"
+                                            ),
+                                            card_body(
+                                                h3(textOutput(outputId="nb_species"))
+                                            )
+                                        )
+                                        )
+                                    ),
+                            )
                         )
                     ),
                     # Retourner la liste des espèces dans une datatable
